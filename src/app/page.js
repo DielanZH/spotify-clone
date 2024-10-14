bf33c574a76eb8708c1c9814e578cdbd65afebcd
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-10-14 19:53:24 -0300

    te voy a embarazar

--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,15 @@ import Main_page from "./components/main_page/main_page";
 export default function Home() {
   return (
     <>
-    <Nav_bar></Nav_bar>
-    <div className="grid grid-cols-2">
-      <Biblioteca></Biblioteca>
 
-      <Main_page></Main_page>
+    <Nav_bar />
+
+    <div className="grid grid-cols-2">
+      <Biblioteca />
+
+      <Main_page />
     </div>
+    
     </>
   );
 }
