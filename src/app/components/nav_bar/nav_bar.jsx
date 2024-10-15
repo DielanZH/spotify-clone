9d2f9e46206aa3b5f7e8fa34414197f26f54fcb7
emmituwu <noizyclearfan@gmail.com>
2024-10-14 22:58:27 -0300

    te amo toma los cambios peton

--- src/app/components/nav_bar/nav_bar.jsx
+++ src/app/components/nav_bar/nav_bar.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { FaSpotify } from "react-icons/fa";
+import { GoHome } from "react-icons/go";
+import { FaHeart } from "react-icons/fa";
+import { GoSearch } from "react-icons/go";
 
 function Nav_bar() {
   return (
-    <div className='grid grid-cols-3'>
-      <div>asjkdhjkahda</div>
-      <div>asmkjdklasdsa</div>
+
+    <div className='grid grid-cols-3 h-20'>
+
+      <FaSpotify size={40}/>
+
+      <div>
+        <div><GoHome size={40}/></div>
+
+        <div className='flex'>
+          <button><GoSearch /></button>
+
+          <input className="flex-auto max-w-96" type="text" placeholder='¿Qué quieres reproducir?'/>
+
+          <button><FaHeart /></button>
+
+        </div>
+      </div>
+
       <div>asjkdhjksad</div>
+
     </div>
   )
 }
