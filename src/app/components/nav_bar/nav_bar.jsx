a1f70e20fa604323439f314a6a11e78dc1a7a4f5
emmituwu <noizyclearfan@gmail.com>
2024-10-15 23:39:27 -0300

    ya casi todo uwu

--- src/app/components/nav_bar/nav_bar.jsx
+++ src/app/components/nav_bar/nav_bar.jsx
@@ -14,11 +14,13 @@ function Nav_bar() {
     <div className='grid grid-cols-3 h-16 items-center'>
 
       {/* Logo */}
-      <div className='flex sm:justify-start ml-7 -mt-0.5'>
-        <FaSpotify size={33}/>
+      <div title="Spotify" className='flex sm:justify-start ml-7 -mt-0.5'>
+        <button>
+          <FaSpotify size={33}/>
+        </button>
       </div>
 
-      <div className='flex items-center justify-center space-x-2 px-3'>
+      <div title="Inicio" className='flex items-center justify-center space-x-2 px-3'>
         {/* Botón de Home */}
         <div className='bg-[#212121] rounded-full p-2.5 flex items-center justify-center transition-transform duration-200 hover:scale-105 '>
             <button>
@@ -30,7 +32,7 @@ function Nav_bar() {
         <div className={`flex items-center justify-center gap-3 bg-[#212121] rounded-full p-2.5  w-[77%] transition-all duration-300 ${onFocus? "ring-2 ring-[#B3B3B3] border-[#B3B3B3]": "hover:ring-1 hover:ring-[#B3B3B3] hover:border-[#B3B3B3] hover:bg-[#2C2C2C]"} `}>
 
           {/* Botón de búsqueda */}
-          <button className='flex items-center justify-center h-full min-h-4 min-w-4'>
+          <button title="Buscar" className='flex items-center justify-center h-full min-h-4 min-w-4'>
             <GoSearch size={27}/>
           </button>
 
@@ -42,7 +44,7 @@ function Nav_bar() {
           />
 
           {/* Botón de corazón */}
-          <button className='flex items-center justify-center min-h-4 min-w-4'>
+          <button title="Explorar" className='flex items-center justify-center transition-transform duration-200 hover:scale-110 min-h-4 min-w-4'>
             <FaHeart size={25} className='flex items-center justify-center'/>
           </button>
           
@@ -51,10 +53,10 @@ function Nav_bar() {
       </div>
 
       <div className='flex justify-end space-x-8 px-1 '>
-        <button className='text-[#B3B3B3] transition-transform duration-100 hover:scale-105'>
+        <button className='text-[#B3B3B3] transition-transform duration-75 hover:scale-110 hover:text-[#fff]'>
             Registrarte
         </button>
-        <button className='text-[#121212] rounded-full py-3 px-8 bg-[#fff] transition-transform duration-100 hover:scale-105'>
+        <button className='text-[#121212] rounded-full py-3 px-8 bg-[#fff] transition-transform duration-75 hover:scale-105'>
             Iniciar Sesión
         </button>
       </div>
