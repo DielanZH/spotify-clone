0843c94f31d0cd5914960a03c05ab182666b566f
emmituwu <emmaseo@rocketmail.com>
2024-10-20 23:13:03 -0300

    main lado derecho

--- src/app/components/main_page/main_page.jsx
+++ src/app/components/main_page/main_page.jsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
+
 
 function Main_page() {
   const divs = Array(10).fill(null).map((_, index) => (
-    <div className='w-72 h-40 p-3 m-3 bg-red-400 rounded-lg' key={index}>
-    uwu
+    <div className='w-full h-40 p-3 m-3 g-4 bg-red-400 text-2xl rounded-lg' key={index}>
+      <h1>Lista de éxitos</h1>
     </div>
     
   ));
   return (
     <div className='bg-primaryColor rounded-lg p-4'>
       <h2 className='mt-11 text-2xl'>Explorar todo</h2>
-      <div className='grid grid-cols-5'>{divs}</div>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 '>{divs}</div>
+      
     </div>
   )
 }
