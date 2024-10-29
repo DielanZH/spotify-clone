ee6ea088cbe540d8ca490000ac05a25b0b15df23
emmituwu <emmaseo@rocketmail.com>
2024-10-29 19:41:13 -0300

    hola :3

--- src/app/components/main_page/main_page.jsx
+++ src/app/components/main_page/main_page.jsx
@@ -1,38 +1,47 @@
-import Image from 'next/image';
-import React from 'react';
+"use client";
 
+import Image from 'next/image';
+import React, { useState, useEffect } from 'react';
 
 function Main_page() {
+  const [isSmallScreen, setIsSmallScreen] = useState(false);
+
+  // Actualiza el estado según el tamaño de la pantalla
+  useEffect(() => {
+    const handleResize = () => {
+      setIsSmallScreen(window.innerWidth < 768); // Considera pantallas menores a 768px como pequeñas
+    };
+
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Verifica el tamaño inicial
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   const divs = Array(10).fill(null).map((_, index) => (
-<<<<<<< HEAD
-    <div className='relative w-full h-48 p-3 g-4 bg-red-400 text-2xl rounded-lg overflow-hidden' key={index}>
-=======
-    <div className='w-full max-w-80 h-40 p-3 m-3 g-4 bg-red-400 text-2xl rounded-lg' key={index}>
->>>>>>> 39b0ce3a86d9010a814b48abacd24f4d96338b25
+    <div 
+      className={`relative p-3 g-4 ${isSmallScreen ? 'h-64' : 'h-48'} w-full bg-red-400 text-2xl rounded-lg overflow-hidden`}
+      key={index}
+    >
       <h1>Lista de éxitos</h1>
       <Image
         src="/images/ariana.jpg"
-        width={150} // Cambia estos valores al tamaño que quieras
+        width={150} 
         height={150}
         alt="Imagen de Ariana"
         className="absolute -right-9 top-12 rounded-md transform rotate-[18deg]"
       />
     </div>
-
   ));
+
   return (
-<<<<<<< HEAD
-    <div className='bg-primaryColor rounded-lg p-4'>
-      <h2 className='mt-11 mb-5 text-2xl'>Explorar todo</h2>
-      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 '>{divs}</div>
-      
-=======
     <div className='bg-primaryColor rounded-lg p-4 max-h-[77vh]'>
-      <h2 className='mt-11 text-2xl'>Explorar todo</h2>
-      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 max-h-[61vh] overflow-y-auto overflow-x-hidden'>{divs}</div>
->>>>>>> 39b0ce3a86d9010a814b48abacd24f4d96338b25
+      <h2 className='mt-11 mb-4 text-2xl'>Explorar todo</h2>
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 max-h-[61vh] overflow-y-auto overflow-x-hidden'>
+        {divs}
+      </div>
     </div>
-  )
+  );
 }
 
-export default Main_page
+export default Main_page;
