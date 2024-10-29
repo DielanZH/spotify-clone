39b0ce3a86d9010a814b48abacd24f4d96338b25
Daniel Diaz <danieldiaz-123@hotmail.com>
2024-10-29 16:37:35 -0300

    barra eje Y

--- src/app/components/main_page/main_page.jsx
+++ src/app/components/main_page/main_page.jsx
@@ -3,16 +3,15 @@ import React from 'react';
 
 function Main_page() {
   const divs = Array(10).fill(null).map((_, index) => (
-    <div className='w-full h-40 p-3 m-3 g-4 bg-red-400 text-2xl rounded-lg' key={index}>
+    <div className='w-full max-w-80 h-40 p-3 m-3 g-4 bg-red-400 text-2xl rounded-lg' key={index}>
       <h1>Lista de éxitos</h1>
     </div>
     
   ));
   return (
-    <div className='bg-primaryColor rounded-lg p-4'>
+    <div className='bg-primaryColor rounded-lg p-4 max-h-[77vh]'>
       <h2 className='mt-11 text-2xl'>Explorar todo</h2>
-      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 '>{divs}</div>
-      
+      <div className='grid grid-cols-[repeat(auto-fill,minmax(16rem,1fr))] gap-6 max-h-[61vh] overflow-y-auto overflow-x-hidden'>{divs}</div>
     </div>
   )
 }
